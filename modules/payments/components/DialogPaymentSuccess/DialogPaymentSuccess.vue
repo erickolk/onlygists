3677f8f87e36717d24e0d2d2dc1405ef30564dd3
Erick Souza <erick.erick669@gmail.com>
2024-06-24 15:22:15 -0300

    Feat: criação dos modais de confirmação de pagamento e de erro no pagamento

---
+++ modules/payments/components/DialogPaymentSuccess/DialogPaymentSuccess.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+const visible = defineModel<boolean>('visible')
+</script>
+
+<template>
+  <Dialog v-model:visible="visible" header="Compra efetuada com sucesso!" :style="{ width: '400px' }">
+    <div class="flex flex-col items-center m-0 justify-center w-full">
+      <i class="pi pi-check-circle text-primary-500 text-6xl my-5"></i>
+      <p class="font-[Inter] text-gray-700 text-center">
+        Parabéns pela compra! Você receberá no email fornecido no checkout o seu gist 🤩
+      </p>
+    </div>
+
+    <template #footer>
+        <div class="flex justify-center w-full">
+            <Button label="Voltar" icon="pi pi-arrow-left" @click="visible = false" autofocus/>
+        </div>
+    </template>
+  </Dialog>
+</template>
