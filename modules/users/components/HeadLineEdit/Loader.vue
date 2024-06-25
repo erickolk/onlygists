412a53c224e9b16312549d938fbd753e60a35fb2
Erick Souza <erick.erick669@gmail.com>
2024-06-25 14:03:47 -0300

    Feat: Criação do layout de checkout, Loader de Edit e componente de Edit profile

---
+++ modules/users/components/HeadLineEdit/Loader.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps<{
+  loading: boolean
+}>()
+</script>
+
+<template>
+  <div class="flex flex-col items-center gap-2 py-8" v-if="props.loading">
+    <Skeleton shape="circle" size="10rem" />
+    <Skeleton width="18rem" height="2rem" />
+    <div class="flex gap-2">
+      <Skeleton width="8rem" height="2rem" />
+      <Skeleton width="8rem" height="2rem" />
+    </div>
+  </div>
+
+  <slot v-else />
+</template>
---
+++ modules/users/screens/EditProfile/EditProfile.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import HeadLineEdit from '@/modules/users/components/HeadLineEdit/HeadLineEdit.vue'
+import HeadLineEditLoader from '@/modules/users/components/HeadLineEdit/Loader.vue'
+import type { MySelfContextProvider } from '~/modules/users/composables/useMySelf/types'
+import { mySelfKey } from '~/modules/users/composables/useMySelf/useMySelf'
+
+const { user, loading } = inject(mySelfKey) as MySelfContextProvider
+</script>
+
+<template>
+  <HeadLineEditLoader :loading="loading">
+    <HeadLineEdit
+      @share="handleShare"
+      @navigate-to-profile="handleNavigateToProfile"
+      class="my-10"
+      :username="user?.username"
+      :avatar-url="user?.avatarUrl"
+    />
+  </HeadLineEditLoader>
+</template>
